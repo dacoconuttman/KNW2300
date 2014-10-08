5a694e8e8de91496f3034da17d47c62d6e4de80b
dacoconuttman <dacoconuttman@gmail.com>
2014-10-08 17:58:25 -0500

    7
    defined left and right

--- Knw2300.java
+++ Knw2300.java
@@ -9,16 +9,22 @@ import rxtxrobot.*;
  *
  * @author Ashley
  */
-public class Knw2300 {
+public class Robot {
     
+	 static int right = RXTXRobot.MOTOR1;
+	 static int left = RXTXRobot.MOTOR2;
     public static void main(String[] args) {        
         RXTXRobot r = new ArduinoNano();
-        r.setPort("/dev/tty.wch ch341 USB=>RS232 fd120");
+        r.setPort("/dev/tty.wch ch341 USB=>RS232 1420");
         r.connect(); 
-        runBothMotors(r);
+        //runBothMotors(r);
         //ping(r);
         //servo(r);
         //shaftEncoder(r);
+        //runBothMotorsBump(r);
+        //bumpSensor(r);
+        runBothMotors(r);
+        //ping(r);
         r.close(); 
     }
     
@@ -30,7 +36,7 @@ public class Knw2300 {
     public static void ping(RXTXRobot r) {
         // >15cm & <15cm
         final int PING_PIN = 4;
-        for (int x=0; x < 10; ++x) 
+        for (int x=0; true; ++x) 
 	{ 
             //Read the ping sensor value, which is connected to pin 12 
             System.out.println("Response: " + r.getPing(PING_PIN) + " cm"); 
@@ -49,20 +55,23 @@ public class Knw2300 {
     
     public static void shaftEncoder(RXTXRobot r) {
         // Turn for 500 ticks
-        System.out.println(r.getEncodedMotorPosition(RXTXRobot.MOTOR1));
-        r.runEncodedMotor(RXTXRobot.MOTOR1, 125, 500);
-        System.out.println(r.getEncodedMotorPosition(RXTXRobot.MOTOR1));
+        while (true){
+        	System.out.println(r.getEncodedMotorPosition(RXTXRobot.MOTOR1));
+            System.out.println(r.getEncodedMotorPosition(RXTXRobot.MOTOR2));
+            r.sleep(500);
+        }
+    	
     }
     
     public static void runBothMotors(RXTXRobot r) {
-        r.runEncodedMotor(RXTXRobot.MOTOR1, -250, 4581, RXTXRobot.MOTOR2, 250, 4581); // Run both motors forward for 3m 
-	r.sleep(5000); // Pause execution for 5 seconds, but the motors keep running. 
-	r.runMotor(RXTXRobot.MOTOR1,0,RXTXRobot.MOTOR2,0,0); // Stop both motors 
+        r.runEncodedMotor(right, -165, 1350, left, 140, 1250); // Run both motors forward for 3m 
+        r.sleep(5000); // Pause execution for 5 seconds, but the motors keep running. 
+        r.runMotor(RXTXRobot.MOTOR1,0,RXTXRobot.MOTOR2,0,0); // Stop both motors 
     }
     
     public static void runBothMotors(RXTXRobot r, int x){
     	//run motor for x meters
-        r.runEncodedMotor(RXTXRobot.MOTOR1, -250, 1527*x, RXTXRobot.MOTOR2, 250, 1527*x); // Run both motors forward for 3m 
+        r.runEncodedMotor(right, -150, 1527*x, left, 150, 1527*x); // Run both motors forward for 3m 
 	r.sleep(5000); // Pause execution for 5 seconds, but the motors keep running. 
 	r.runMotor(RXTXRobot.MOTOR1,0,RXTXRobot.MOTOR2,0,0); // Stop both motors 
     }
@@ -70,7 +79,11 @@ public class Knw2300 {
     public static void bumpSensor(RXTXRobot r) {
         // Motor until bump sensor is tapped
         r.refreshAnalogPins();
-        System.out.println(r.getAnalogPin(0));
+        while (true){
+        	r.refreshAnalogPins();
+        	System.out.println(r.getAnalogPin(0));
+        	r.sleep(500);
+        }
         //r.getAnalogPin(1);
         //r.getAnalogPin(2);
         //r.getAnalogPin(3);
@@ -78,17 +91,17 @@ public class Knw2300 {
     
     public static void runBothMotorsBump(RXTXRobot r) {
         // Motor until bump sensor is tapped        
-        r.runMotor(RXTXRobot.MOTOR1, 500, RXTXRobot.MOTOR2, 500, 0); // Run both motors forward indefinitely 
+        r.runMotor(right, -150, left, 150, 0); // Run both motors forward indefinitely 
         while(true){	
         	r.refreshAnalogPins();
         	int bump0 = r.getAnalogPin(0).getValue();
-            int bump1 = r.getAnalogPin(1).getValue();
-            int bump2 = r.getAnalogPin(2).getValue();
-            int bump3 = r.getAnalogPin(3).getValue();
-        	if(bump0!=0||bump1!=0||bump2!=0||bump3!=0){
+            //int bump1 = r.getAnalogPin(1).getValue();
+            //int bump2 = r.getAnalogPin(2).getValue();
+            //int bump3 = r.getAnalogPin(3).getValue();
+        	if(bump0>=10){
         		r.runMotor(RXTXRobot.MOTOR1,0,RXTXRobot.MOTOR2,0,0);
         		break;
         	}
-        	r.sleep(50);
         }
-} 
+}
+}
